a1a77d7a91fa5781cfb606653793633164925958
Alex Cazacu <alex@nvent.nl>
2019-03-01 00:45:48 +0100

    Removed the views and ditched the router-view as we only have one view. Refactored the template and layout with semantic HTML. Added normalize.css. Added placeholder for the high level components. Implemented the layout.

---
+++ src/components/AppTabs.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="tabs">
+    <div class="header"></div>
+    <div class="body"></div>
+  </div>
+</template>
+<style scoped lang="scss">
+@import "../style/variables";
+
+.header {
+  background: $greyscale-0;
+}
+</style>
