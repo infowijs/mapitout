982ceab34ec383887e2361b921563a611cd1d0ea
Alex Cazacu <alex@nvent.nl>
2019-03-17 11:14:56 +0100

    Moved the fetch calls into the store to allow for better PWA integration down the road. Pulled out some functionality from the component into directives.

--- src/components/input/LocationAddress.vue
+++ src/components/input/LocationAddress.vue
@@ -1,9 +1,20 @@
 <template>
-  <label ref="rootEl" v-on:keyup="onKeyUp">
-    <div class="label" :class="{ dirty: isDirty }">{{ isDirty ? label : placeholder }}</div>
-    <input ref="input" type="text" :value="query" @input="onInput" />
-    <ul v-if="suggestions.length > 0">
-      <li v-for="suggestion in suggestions" v-bind:key="suggestion.id">
+  <label
+    ref="rootEl"
+    @keydown="onKeyDown"
+    v-toggle-prop-on-document-click="{ propName: 'displaySuggestions' }"
+  >
+    <input
+      type="text"
+      @input="onInput"
+      @focus="onFocus"
+      @blur="onBlur"
+      :value="query"
+      :placeholder="placeholder"
+    />
+    <div v-if="!isFocused" class="mask"></div>
+    <ul v-if="displaySuggestions" @mouseover="onListMouseOver">
+      <li v-for="(suggestion, index) in suggestions" :key="suggestion.id" @click="select(index)">
         <span>{{ suggestion.weergavenaam }}</span>
       </li>
     </ul>
@@ -12,57 +23,53 @@
 <style scoped lang="scss">
 @import "../../style/variables";
 
-$reserved-label: 16px;
-
 label {
-  display: block;
+  display: flex;
+  flex-direction: row;
   position: relative;
-  padding-top: $reserved-label;
-  margin-bottom: $reserved-label;
   color: $greyscale-1;
 }
 
-div {
+.mask {
+  content: " ";
+  display: block;
   position: absolute;
-  top: $reserved-label + 1px;
-  z-index: 1;
-  font-size: 16px;
-  line-height: 22px;
-  padding: 3px 4px;
-  color: $greyscale-2;
-  transition: transform 0.1s ease-in-out, font-size 0.1s ease-in-out, color 0.1s ease-in-out;
-
-  &.dirty {
-    font-size: 12px;
-    line-height: $reserved-label;
-    color: $greyscale-1;
-    transform: translateY(-$reserved-label);
-  }
+  top: 0;
+  right: 0;
+  width: 25%;
+  height: calc(100% - 2px);
+  background: linear-gradient(to right, rgba(255, 255, 255, 0), white);
 }
 
 input {
-  font-size: 16px;
-  line-height: 22px;
-  padding: 3px 4px;
-  border-width: 0 0 1px 0;
-  border-color: $greyscale-1;
-  color: $greyscale-1;
-  width: calc(100% - 8px);
+  position: relative;
+  font-size: 12px;
+  line-height: 1.83px;
+  border-width: 0 0 2px 0;
+  border-color: #4d4d4d;
+  color: #4d4d4d;
+  outline: none;
+  flex-grow: 1;
 }
 ul {
   position: absolute;
   top: 100%;
-  width: calc(100% - 2px);
-  border-width: 0 1px 1px 1px;
+  width: 100%;
+  border-width: 0;
   border-style: solid;
   border-color: $greyscale-1;
+  border-radius: 0 0 3px 3px;
   background-color: white;
   margin: 0;
   padding: 0;
   list-style: none;
+  box-shadow: 0 2px 4px 0 $greyscale-2;
 
   li {
     padding: 4px 8px;
+    font-size: 12px;
+    line-height: 1.83;
+    cursor: pointer;
 
     &.focused {
       background-color: $greyscale-2;
@@ -72,99 +79,172 @@ ul {
 </style>
 <script>
 import { debounce } from "lodash";
+import { mapActions } from "vuex";
+
+import "../../directives/togglePropOnDocumentClick";
 
 export default {
   props: {
-    label: {
-      type: String,
-      default: "Address"
-    },
     placeholder: {
       type: String,
       default: "Choose an address"
     },
-    datasource: {
-      type: String,
-      default: "https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest"
-    },
-    value: String
+    value: {
+      type: Object,
+      default: function() {
+        return {
+          value: "",
+          coordinates: {
+            lat: 0,
+            lng: 0
+          }
+        };
+      }
+    }
   },
   data() {
     return {
       query: "",
+      isFocused: false,
       isDirty: false,
       isLoading: false,
+      displaySuggestions: false,
       suggestions: []
     };
   },
+
+  mounted() {
+    this.query = this.value.value;
+  },
+
   methods: {
+    ...mapActions(["reportError"]),
+
+    ...mapActions("address", ["search", "resolve"]),
+
+    onFocus() {
+      this.isFocused = true;
+    },
+
+    onBlur() {
+      this.isFocused = false;
+    },
+
+    onListMouseOver(event) {
+      const listEl = event.target.closest("li");
+
+      if (listEl && !listEl.classList.contains("focused")) {
+        this.$refs.rootEl.querySelectorAll("ul li").forEach(listEl => {
+          listEl.classList.remove("focused");
+        });
+
+        listEl.classList.add("focused");
+      }
+    },
+
+    onKeyDown(event) {
+      if (!["ArrowUp", "ArrowDown", "Enter", "Esc"].includes(event.key)) {
+        return;
+      }
+
+      event.preventDefault();
+
+      const suggestionEls = this.$refs.rootEl.querySelectorAll("ul li");
+
+      const currentIndex = Array.prototype.indexOf.call(
+        suggestionEls,
+        this.$refs.rootEl.querySelector("li.focused")
+      );
+
+      if (["ArrowUp", "ArrowDown"].includes(event.key)) {
+        const minIndex = 0;
+        const maxIndex = suggestionEls.length - 1;
+        let nextIndex;
+
+        switch (event.key) {
+          case "ArrowUp":
+            nextIndex =
+              currentIndex === -1
+                ? minIndex
+                : currentIndex - 1 < minIndex
+                ? minIndex
+                : currentIndex - 1;
+            break;
+          case "ArrowDown":
+            nextIndex =
+              currentIndex === -1
+                ? minIndex
+                : currentIndex + 1 > maxIndex
+                ? maxIndex
+                : currentIndex + 1;
+            break;
+        }
+
+        if (currentIndex !== nextIndex) {
+          if (currentIndex !== -1) {
+            suggestionEls.item(currentIndex).classList.remove("focused");
+          }
+          suggestionEls.item(nextIndex).classList.add("focused");
+        }
+      }
+
+      if (event.key === "Enter") {
+        if (currentIndex !== -1) {
+          this.select(currentIndex);
+        } else if (this.query !== this.value.value) {
+          this.$emit("input", { value: "", coordinates: null });
+        }
+      }
+
+      if (event.key === "Esc") {
+        this.displaySuggestions = false;
+      }
+    },
+
     onInput(event) {
       const value = event.target.value;
 
       this.isDirty = value.length > 0;
 
-      this.$refs.input.focus();
+      this.query = value;
 
-      if (value.length === 0) {
-        this.$emit("input", value);
-      } else if (value.length >= 3) {
-        this.query = value;
+      if (value.length > 2) {
+        this.suggest(value);
+      } else {
+        this.suggestions = [];
+        this.displaySuggestions = false;
       }
     },
 
-    onKeyUp(event) {
-      switch (event.key) {
-        case "ArrowUp":
-          event.preventDefault();
-          break;
-        case "ArrowDown":
-          event.preventDefault();
-          break;
-        case "Enter":
-          event.preventDefault();
-          break;
-      }
-    },
-
-    async suggest(query) {
-      this.suggestions = await this.fetchSuggestions(query);
-
-      console.log(this.suggestions);
-    },
-
-    async fetchSuggestions(query) {
-      const url = new URL(this.datasource);
-
-      url.searchParams.append("fq", "type:adres");
-      url.searchParams.append("q", query);
-      url.searchParams.append("rows", 5);
-
-      const request = {
-        method: "GET"
-      };
-
+    suggest: debounce(async function(query) {
       this.isLoading = true;
-      const response = await fetch(url, request);
 
-      if (!response.ok) {
-        this.isLoading = false;
-        throw new Error(response.statusText);
+      this.suggestions = await this.search(query);
+
+      this.isLoading = false;
+
+      if (this.suggestions.length > 0) {
+        this.displaySuggestions = true;
+      }
+    }, 500),
+
+    async select(index) {
+      const address = this.suggestions[index];
+      const coords = await this.resolve(address.id);
+
+      if (coords) {
+        const value = {
+          value: address.weergavenaam,
+          coordinates: coords
+        };
+
+        this.query = value.value;
+
+        this.$emit("input", value);
       }
 
-      try {
-        const result = await response.json();
-        return result.response.docs;
-      } catch (error) {
-        throw new Error("Invalid response format");
-      } finally {
-        this.isLoading = false;
-      }
+      this.displaySuggestions = false;
     }
-  },
-  watch: {
-    query: debounce(function(query) {
-      this.suggest(query);
-    }, 500)
   }
 };
 </script>
