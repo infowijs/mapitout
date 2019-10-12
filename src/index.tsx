6bdfabbeeccf4aeac5efada864e3c4464a615d0b
Mike Duister <mike@duister.me>
2019-10-12 18:40:32 +0200

    Implemented the new packages and configuration.
    Support for environments, redux and the router.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { configureStore } from './store'
+import { App } from './App'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import '../node_modules/normalize.css/normalize.css'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const { store, persistor } = configureStore()
+
+ReactDOM.render((
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <BrowserRouter>
+                <Route component={App} path='/:travelOne?/:travelTwo?/:travelThree?/:travelFour?/:travelFive?/:travelSix?'/>
+            </BrowserRouter>
+        </PersistGate>
+    </Provider>
+), document.getElementById('root'))
