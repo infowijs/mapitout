c2523ad9300dbdafea335d9861f7d80d078372f0
Mike Duister <mike@duister.me>
2019-10-12 18:30:10 +0200

    Added a `TravelType` enum.

---
+++ src/enums/TravelType.ts
@@ -0,0 +1,7 @@
+export enum TravelType {
+    PublicTransport = 'public-transport',
+    Driving = 'driving',
+    Cycling = 'cycling',
+    Walking = 'walking',
+    PublicTransportAndCycling = 'cycling+ferry'
+}
