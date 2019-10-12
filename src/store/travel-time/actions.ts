c49492f53dd0de03d48344699fcc19fb9e77b280
Mike Duister <mike@duister.me>
2019-10-12 18:37:12 +0200

    Implemented a `getTravelTimes` dispatcher.

--- src/store/travel-time/actions.ts
+++ src/store/travel-time/actions.ts
@@ -1,5 +1,9 @@
+import moment from 'moment'
+import { AxiosResponse } from 'axios'
 
+import { api } from '../../utils'
 import { ActionDispatch } from './reducer'
+import { TravelTimeAbstraction, TravelTimeBody, TravelTimeResponse } from '../../interfaces'
 
 export enum ActionType {
     SetLoading = '@@travel-time/loading',
@@ -7,3 +11,58 @@ export enum ActionType {
     PurgeTravelTimes = '@@travel-time/purge'
 }
 
+// NOTE: Since the API calculates the overlapping areas of all travels every time a new one is added, all the travels
+// should be passed to the API and therefor also this function. The parameters of this function serve as an abstraction
+// layer on top of the desired body format.
+export function getTravelTimes(travels: TravelTimeAbstraction[]) {
+	const travelsWithIds: Array<TravelTimeAbstraction & {id: string}> = travels.map((travel, i) => ({
+		...travel,
+		id: i.toString()
+	}))
+
+    const departure_time = moment() // Get the current data
+            .utc() // Remove timezone details by setting it to UTC
+            .day(8) // Set the date to the date of next monday
+            .hour(9).minute(0).second(0).millisecond(0) // Set the time to 9:00 (AM)
+            .format() // Turn the moment object into a string
+
+    const data: TravelTimeBody = {
+        departure_searches: travelsWithIds.map((travel, i) => ({
+            id: travel.id,
+            coords: travel.location,
+            departure_time,
+            travel_time: travel.duration,
+            transportation: {
+                type: travel.transport
+            }
+        })),
+        unions: [
+            {
+                id: 'union',
+                search_ids: []
+            }
+        ]
+    }
+
+    data.unions[0].search_ids = data.departure_searches.map((search) => search.id)
+
+    return (dispatch: (action: ActionDispatch) => {}) => {
+        dispatch({
+            type: ActionType.SetLoading
+        })
+
+        api.post('/TravelTimeApi/v4/time-map', data)
+            .then((res: AxiosResponse<TravelTimeResponse>) => {
+                dispatch({
+                    type: ActionType.GetTravelTimes,
+                    data: {
+                        travelTimes: travelsWithIds.map((travel) => ({
+                            ...travel,
+                            res: res.data.results.filter((result) => result.search_id === travel.id)[0]
+                        })),
+                        union: res.data.results.filter((result) => result.search_id === 'union')[0]
+                    }
+                })
+            })
+    }
+}
