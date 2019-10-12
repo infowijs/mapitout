c4872d7cc200947ade00ed23b71884fa9f570d87
Mike Duister <mike@duister.me>
2019-10-12 18:42:20 +0200

    Rewrote the App's base.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators, Dispatch } from 'redux'
+import { RouteComponentProps } from 'react-router-dom'
+import styled from 'styled-components'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ReduxState, getTravelTimes, purgeTravelTimes } from './store'
+import { TravelType } from './enums'
+
+interface StateProps {
+	loading: ReduxState['travelTime']['loading']
+	travelTimes: ReduxState['travelTime']['travelTimes']
+	overlap: ReduxState['travelTime']['overlap']
+}
+interface DispatchProps {
+	getTravelTimes: typeof getTravelTimes
+	purgeTravelTimes: typeof purgeTravelTimes
+}
+interface Props {}
+type PropsUnion = StateProps & DispatchProps & Props & RouteComponentProps<Params>
+
+interface State {}
+
+interface Params {
+	travelOne: string
+	travelTwo: string
+	travelThree: string
+	travelFour: string
+	travelFive: string
+	travelSix: string
 }
 
-export default App;
+
+export class Component extends React.Component<PropsUnion, State> {
+	public readonly state: State = {}
+	public render() {
+		const {loading, travelTimes, overlap} = this.props
+
+		return (
+			<div>
+				<h1>App</h1>
+			</div>
+		)
+	}
+}
+
+const mapStateToProps = (state: ReduxState) => ({
+	loading: state.travelTime.loading,
+	travelTimes: state.travelTime.travelTimes,
+	overlap: state.travelTime.overlap
+})
+
+const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators({
+	getTravelTimes,
+	purgeTravelTimes
+}, dispatch)
+
+export const App = connect<StateProps, DispatchProps, Props, ReduxState>(
+	mapStateToProps,
+	mapDispatchToProps
+)(Component)
