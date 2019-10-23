e8a1de49f272d52ff37b89e8b29b27ab3443c375
Mike Duister <mike@duister.me>
2019-10-23 15:53:34 +0200

    Added the "bus" icon.

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,4 @@
 export { ReactComponent as AddIcon } from './add.svg'
 export { ReactComponent as BikeIcon } from './bike.svg'
 export { ReactComponent as BusIcon } from './bus.svg'
+export { ReactComponent as BusBikeIcon } from './bus-bike.svg'
