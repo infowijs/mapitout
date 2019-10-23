9b0d237863a44ebbbc7eac414393a0208a24c643
Mike Duister <mike@duister.me>
2019-10-23 15:50:48 +0200

    Fixed the imports and added the `Map` component as a default rendered component.

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,8 @@ import { PersistGate } from 'redux-persist/integration/react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import dotenv from 'dotenv'
 
-import { configureStore } from './store'
-import { App } from './App'
+import { configureStore } from 'store'
+import { Map, App } from 'containers'
 
 import '../node_modules/normalize.css/normalize.css'
 import './index.css'
@@ -18,6 +18,7 @@ const { store, persistor } = configureStore()
 ReactDOM.render((
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
+			<Map/>
             <BrowserRouter>
                 <Route component={App} path='/:travelOne?/:travelTwo?/:travelThree?/:travelFour?/:travelFive?/:travelSix?'/>
             </BrowserRouter>
