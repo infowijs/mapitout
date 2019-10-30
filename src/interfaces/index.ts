9f37c3686a941e10956cde7ba657af90da860ecf
Mike Duister <mike@duister.me>
2019-10-31 00:10:58 +0100

    Added an interface describing the POI API's response.

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,4 @@
+import { POIApiResponse } from './api/POIApiResponse'
 import { TravelTimeBody } from './api/TravelTimeBody'
 import { TravelTimeResponse } from './api/TravelTimeResponse'
 
@@ -7,6 +8,7 @@ import { TravelTimeStored } from './TravelTimeStored'
 // NOTE: Normally barrel files can just re-export types, unfortunately CRA always enables the --isolatedModules flag,
 // causing a TS1205 error. A workaround is to cast them to a type with the same name and export that.
 
+export type POIApiResponse = POIApiResponse
 export type TravelTimeBody = TravelTimeBody
 export type TravelTimeResponse = TravelTimeResponse
 
