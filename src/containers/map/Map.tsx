3a6ea08bcaa62fc0e85beae33eb4181142357947
Mike Duister <mike@duister.me>
2019-11-04 00:05:53 +0100

    Added an animated fit to bounds on travelTimes change.

--- src/containers/map/Map.tsx
+++ src/containers/map/Map.tsx
@@ -38,6 +38,7 @@ const StyledZoomControlButton = styled.div`
 `
 
 interface StateProps {
+	travelTimes: ReduxState['travelTime']['travelTimes']
 	tooltip: ReduxState['application']['tooltip']
 }
 interface DispatchProps {
@@ -54,6 +55,44 @@ export class Component extends React.Component<PropsUnion, State> {
 	public mapRef = React.createRef<GoogleMap>()
 
 	public setTooltipTimeout: ReturnType<typeof setTimeout> | null = null
+
+	public shouldComponentUpdate(nextProps: Readonly<PropsUnion>, nextState: Readonly<State>, nextContext: any): boolean {
+		if (this.mapRef.current && nextProps.travelTimes && nextProps.travelTimes !== this.props.travelTimes) {
+			let north = 0
+			let east = 0
+			let south = 99
+			let west = 99
+
+			for (const coordinate of nextProps.travelTimes.map((v) => v.res.shapes.map((s) => s.shell)).flat(2)) {
+				north = Math.max(north, coordinate.lat)
+				east = Math.max(east, coordinate.lng)
+				south = Math.min(south, coordinate.lat)
+				west = Math.min(west, coordinate.lng)
+			}
+
+			const zoomLevel = Math.min(getBoundsZoomLevel(
+				new google.maps.LatLngBounds(
+				{lat: south, lng: west},
+				{lat: north, lng: east}
+				),
+				{
+					width: window.innerWidth,
+					height: window.innerHeight
+				}), 12)
+
+			if (this.mapRef.current.getZoom() !== zoomLevel) {
+				this.zoomTo(this.mapRef.current.getZoom(), zoomLevel, this.mapRef.current.getZoom() > zoomLevel ? 'out' : 'in')
+			}
+
+			this.mapRef.current.panTo({
+				lat: (north + south) / 2,
+				lng: (east + west) / 2
+			})
+		}
+
+		return false
+	}
+
 	public render() {
 		const MapFactory = withGoogleMap((props: any) =>
 			<GoogleMap
@@ -169,7 +208,36 @@ export class Component extends React.Component<PropsUnion, State> {
 	}
 }
 
+function getBoundsZoomLevel(bounds: google.maps.LatLngBounds, mapDim: {width: number, height: number}): number {
+	const WORLD_DIM = { height: 256, width: 256 };
+	const ZOOM_MAX = 21;
+
+	function latRad(lat: number) {
+		const sin = Math.sin(lat * Math.PI / 180);
+		const radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
+		return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
+	}
+
+	function zoom(mapPx: number, worldPx: number, fraction: number) {
+		return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
+	}
+
+	const ne = bounds.getNorthEast();
+	const sw = bounds.getSouthWest();
+
+	const latFraction = (latRad(ne.lat()) - latRad(sw.lat())) / Math.PI;
+
+	const lngDiff = ne.lng() - sw.lng();
+	const lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
+
+	const latZoom = zoom(mapDim.height, WORLD_DIM.height, latFraction);
+	const lngZoom = zoom(mapDim.width, WORLD_DIM.width, lngFraction);
+
+	return Math.min(latZoom, lngZoom, ZOOM_MAX);
+}
+
 const mapStateToProps = (state: ReduxState) => ({
+	travelTimes: state.travelTime.travelTimes,
 	tooltip: state.application.tooltip
 })
 
