fcb6f3e59563a72de3ed3bbae684b7d57dc88784
Tobias <31845825+tobiasdg@users.noreply.github.com>
2020-06-03 16:48:09 +0200

    Update FAQ naar NL

--- web/src/components/faq/FaqNL.tsx
+++ web/src/components/faq/FaqNL.tsx
@@ -6,112 +6,61 @@ export const FaqNL = ({ setDemoVisibility }: any) => (
 	<>
 		<StyledContentSegment>
 			<h1>Wat is de MapitOut tool?</h1>
-			<p>The MapitOut tool is an interactive map that allows you to select a location to live in by
-				entering desired travel times to different locations. You can find out more about the various
-				parts of the Amsterdam Area on the <a
-					href='https://www.iamsterdam.com/en/living/about-living-in-amsterdam/living-in-the-amsterdam-area'
-					target='_blank' rel='noopener noreferrer'>‘Living in the Amsterdam Area’ section of the I
-					amsterdam website</a>. Internationals who have come to work in the Amsterdam Area can find
-				reliable information about what makes the region unique, including videos and testimonials of
-				people living there.</p>
-			<p>Please note that the tool is not intended for planning travel in real-time. Refer to real-time
-				travel platforms such as <a
-					href='https://9292.nl/en' target='_blank' rel='noopener noreferrer'>www.9292.nl/en</a> for
-				public transport information. </p>
+			<p>De MapitOut tool is een interactieve kaart die helpt mogelijke woonlocaties te vinden door de gewenste max. reistijd naar verschillende locaties in te voeren. NB: de tool is niet bedoeld voor het real-time plannen van een reis, daarvoor kunt u gebruik maken van real-time platforms zoals <a href='https://9292.nl/' target='_blank' rel='noopener noreferrer'>www.9292.nl/</a> voor openbaar vervoer.</p>
+			<p>Internationals die in regio Amsterdam komen werken en de regio nog niet kennen kunnen meer informatie over de verschillende deelgebieden van metropoolregio Amsterdam vinden op het <a href='https://www.iamsterdam.com/en/living/about-living-in-amsterdam/living-in-the-amsterdam-area' target='_blank' rel='noopener noreferrer'>‘Living in the Amsterdam Area’</a>.</p>
 		</StyledContentSegment>
 		<StyledContentSegment>
-			<h1>Who is the tool for?</h1>
-			<p>Anyone interested in learning more about travel times in the Netherlands can use the MapitOut
-				tool! It was designed in English to help internationals who have come to work in the Amsterdam
-				Area, but it also uses symbols and visual elements to assist non-English speakers.</p>
+			<h1>Voor wie is de tool?</h1>
+			<p>Iedereen die meer wil weten over reistijden in Nederland kan de MapitOut tool gebruiken! Gebruikers kunnen voor zowel de Nederlands-  als Engelstalige versie kiezen, daarnaast gebruikt de tool veel symbolen en visuele elementen.</p>
 		</StyledContentSegment>
 		<StyledContentSegment>
-			<h1>How does the tool work?</h1>
-			<p>Say you would want to travel for up to half an hour to your workplace by taking public transport,
-				while at the same time being no further than 15 minutes from your children’s school by bike –
-				MapitOut will then show you the overlapping area. This overlapping area shows potential living
-				areas which meet both travel time criteria. You can continue to add more locations, such as your
-				partner’s workplace or a friend’s home. <span
-					className='link' onClick={() => setDemoVisibility(true)}>Watch the MapitOut tutorial video to see it in action</span>.
+			<h1>Hoe werkt de tool?</h1>
+			<p>Stel je wilt max. een half uur reizen naar werk met het openbaar vervoer, terwijl je niet verder dan 15 minuten met de fiets van de school van je kinderen wilt wonen. MapitOut toont de gebieden voor beide zoekvragen en het overlappende gebied. Dit overlappende gebied toont de potentiële woonlocaties die aan beide criteria voldoen. Je kunt doorgaan locaties toe te voegen, zoals de werklocatie van je partner of het huis van een vriend. <span className='link' onClick={() => setDemoVisibility(true)}>Bekijk het instructiefilmpje om de tool in actie te zien</span>.
 			</p>
 		</StyledContentSegment>
 		<StyledContentSegment>
-			<h1>How can I access the tool?</h1>
-			<p>The MapitOut tool can be accessed through the ‘Living in the Amsterdam Area’ page mentioned above
-				or directly at <a href='https://mapitout.iamsterdam.com' target='_blank'
-								  rel='noopener noreferrer'>https://mapitout.iamsterdam.com</a>.</p>
+			<h1>Hoe krijg ik toegang tot de tool?</h1>
+			<p>De MapitOut tool is te vinden op bovengenoemde ‘Living in the Amsterdam Area’ pagina of direct via <a href='https://mapitout.iamsterdam.com' target='_blank' rel='noopener noreferrer'>https://mapitout.iamsterdam.com</a>.</p>
 		</StyledContentSegment>
 		<StyledContentSegment>
-			<h1>What should I do if the tool isn’t working?</h1>
-			<p>Oh no! First, try refreshing the url or try another browser (e.g. Firefox or Chrome). When
-				searching for a location you can enter an establishment or an address, if so please put a comma
-				between the street address and the city. Always click on the appropriate dropdown suggestion. If
-				you still have issues, contact us by email: <a
-					href='mailto:welcome@amsterdam.nl'>welcome@amsterdam.nl</a> mentioning you have a
-				question/remark regarding the MapitOut tool. Feel free to include print screens and details of
-				your browser.</p>
+			<h1>Wat kan ik doen als de tool niet werkt? </h1>
+			<p>Oh nee! Probeer als eerste de url te vernieuwen of een andere browser te gebruiken (bijv. Firefox of Chrome). Als er nog steeds problemen zijn dan horen wij dat graag via mailadres <a href='mailto:welcome@amsterdam.nl' target='_blank' rel='noopener noreferrer'>welcome@amsterdam.nl</a>, met vermelding opmerking/vraag MapitOut tool (bij voorkeur met printscreen en vermelding van de browser).</p>
 		</StyledContentSegment>
 		<StyledContentSegment>
 			<h1>Which travel information does it use?</h1>
-			<p>Travel times are calculated using various data sources. Google Maps is used to create the base
-				level map to show where paths, roads and stations are located. Public transport timetable data
-				is then layered on top including agencies such as GVB, FlixBus, NS, R-Net and more. Driving,
-				cycling and walking time models are developed by TravelTime platform. <a
-					href='https://www.traveltimeplatform.com/turn-radius-maps-into-travel-time' target='_blank'
-					rel='noopener noreferrer'>Find more information about the TravelTime platform.</a></p>
+			<p>De reistijden worden op basis van verschillende databronnen berekend. OpenStreetMap wordt gebruikt voor het basiniveau waarin wegen, straten en stations worden aangegeven. Daar bovenop komt een laag met openbaar vervoer (OV) dat gebruikt maakt van verschillende reisschema’s incl. van agentschappen zoals NS, GVB, Flixbus, R-net en meer. De reistijd modellen voor rijden, fietsen en lopen zijn ontwikkeld door het Traveltime Plaform. <a href='https://www.traveltimeplatform.com/turn-radius-maps-into-travel-time' target='_blank' rel='noopener noreferrer'>Zie Traveltime voor meer informatie.</a></p>
 		</StyledContentSegment>
 		<StyledContentSegment>
-			<h1>How is the reach of a cycling and public transport search determined?</h1>
-			<p>The tool assumes the user can cycle at the start of each journey, and that residents can cycle
-				for up to 10 minutes to reach their first public transport connection. It also allows five
-				minutes for the cyclist to park their bike. It also assumes users are willing to walk to an
-				initial transport mode (if quicker) and walk to transfer between modes.</p>
-			<p>Once the traveller takes a mode of public transport, such as a train, the tool assumes travel
-				continues via public transport for the remainder of the journey. The only exception to this is
-				ferries because residents often take bikes with them on this mode.</p>
-			<p>Some example journeys within 30 minutes</p>
+			<h1>Hoe wordt het bereik met de fiets of het openbaar vervoer bepaald?</h1>
+			<p>De tool gaat ervan uit dat de gebruiker bij de start van de reis gebruik kan maken van een fiets, en dat men max. 10 minuten fietst tot het OV. Voor het parkeren van de fiets wordt 5 minuten gerekend. Ook wordt ervan uitgegaan dat gebruikers, als dat sneller gaat, bereid zijn naar een beginpunt of tussen twee OV verbindingen te lopen.
+</p>
+			<p>Als de reiziger eenmaal een vorm van OV pakt zoals de trein, gaat de tool ervan uit dat de reiziger voor de rest van de tijd met het OV reist. De enige uitzondering hierop is de veerpont daar bewoners vaak de fiets daarop meenemen. Voorbeelden van een reis binnen 30 minuten:
+</p>
 			<ul>
-				<li>Cycle 8 minutes + park 5 minutes + walk 5 minutes + metro 12 minutes (30 minutes)</li>
-				<li>Cycle 10 minutes, park 5 minutes + train 15 minutes (30 minutes)</li>
-				<li>Walk 12 minutes (12 minutes – still reachable in under 30 minutes)</li>
+				<li>Fiets 8 minuten + parkeer 5 minuten + loop 5 minuten + metro 12 minuten (30 minuten)</li>
+				<li>Fiets 10 minuten, parkeer 5 minuten + trein 15 minuten (30 minuten)</li>
+				<li>Loop 12 minuten (12 minuten – bereikbaar binnen 30 minuten)</li>
 			</ul>
 		</StyledContentSegment>
 		<StyledContentSegment>
-			<h1>I would like to offer this tool as well, is that possible?</h1>
-			<p>That is great, maybe we can help. You can always refer to the MapitOut tool. However, you might
-				want to apply the functionality of the tool to your own website. The application is built as
-				open-source, which means you can fork the tool. To make the tool work like the MapitOut tool,
-				you do need to close separate data contracts (e.g. the TravelTime platform). You can find the
-				information required on <a
-					href='https://github.com/infowijs/mapitout' target='_blank' rel='noopener noreferrer'>Github</a>.
+			<h1>Ik wil deze tool ook aanbieden, kan dat?</h1>
+			<p>Dat is geweldig, misschien kunnen wij daarbij helpen. Je kunt altijd verwijzen naar de MapitOut tool. Maar misschien wil je de functionaliteit van de tool (bereik binnen bepaalde reistijd met bepaald vervoersmiddel) toepassen op je eigen website. De applicatie is open source ontwikkeld, wat betekent dat je een afgeleide kunt ontwikkelen. Om de tool te laten werken als de MapitOut tool dien je daarbij een eigen data contract  af te sluiten (zoals met TravelTime), nadere informatie vindt je op <a href='https://github.com/infowijs/mapitout' target='_blank' rel='noopener noreferrer'>Github</a>.
 			</p>
 		</StyledContentSegment>
 		<StyledContentSegment>
-			<h1>A few disclaimers</h1>
+			<h1>Een paar disclaimers</h1>
 			<ul>
-				<li>Pinpoints on the map. Due to pinpoints placed on the map by Google which can be further than
-					in reality, your search result could be different than expected. One solution could be that
-					if you get multiple suggestions for e.g. a station (or other location), you try several and
-					see if that changes the result.
+				<li>“Pinpoints” op de kaart. Omdat pinpoints die door Google op de kaart geplaatst zijn soms verder weg liggen dan in werkelijkheid, kan het resultaat van een zoekvraag anders zijn dan verwacht.  Een oplossing kan zijn om als je meerdere suggesties voor bijv. een station krijgt verschillende opties te proberen om te zien of het resultaat daarmee verandert.
 				</li>
-				<li>Please note that the tool is not a real-time travel tool. The tool shows results (ranges)
-					assuming the public transport journey takes place anytime between a timeslot of two hours
-					during the day but does not take into account the exact time you wish to travel. It could be
-					that the actual travel will take longer, or there is no public transport at a certain time,
-					or you will have to wait longer for a train. Please refer to real-time travel platforms to
-					make sure you can reach your destination (e.g. <a
-						href='https://9292.nl/en' target='_blank' rel='noopener noreferrer'>www.9292.nl/en</a>).
+				<li>Houd er rekening mee dat de tool geen real-time tool is. De tool toont het resultaat (bereik) ervan uitgaande dat de reis met het OV plaats vindt op elk moment binnen een bepaalde tijdspanne van twee uur overdag, maar houdt geen rekening met het precieze moment dat je wilt reizen. Het kan zijn dat de eigenlijke reis langer duurt, of dat er geen openbaar vervoer is op een bepaalde tijd, of dat je langer op een trein moet wachten. Ga daarom van  real-time travel platforms om zeker te zijn dat je je bestemming op een specifiek moment kunt bereiken (zoals <a href='https://9292.nl/' target='_blank' rel='noopener noreferrer'>www.9292.nl/</a>).
 				</li>
-				<li>The shape of the ranges (results of your searches) is adjusted with a processing layer. This
-					means the amount of points have been reduced for better performance of the application. The
-					remaining points are smoothed based on the current zoom level.
+				<li>De vorm van het bereik (als resultaat van je zoekvraag) is enigszins aangepast, het aantal punten is beperkt om  deprestatie van de tool te verbeteren. De resterende punten worden strak getrokken tot nette lijn gebaseerd op het niveau van inzoomen.
 				</li>
 			</ul>
 		</StyledContentSegment>
 		<StyledContentSegment>
-			<h1>Questions?</h1>
-			<p>For any questions or suggestions please contact <a
-				href='mailto:welcome@amsterdam.nl' target='_blank' rel='noopener noreferrer'>welcome@amsterdam.nl</a>.
+			<h1>Vragen?</h1>
+			<p>Voor vragen en suggesties ontvangen wij graag graag een mail (met vermelding MapitOut tool) op mailadres <a href='mailto:welcome@amsterdam.nl' target='_blank' rel='noopener noreferrer'>welcome@amsterdam.nl</a>.
 			</p>
 		</StyledContentSegment>
 	</>
